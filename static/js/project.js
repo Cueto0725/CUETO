ac74151686f7b9f055aa578f9ed4ace2d17c3e46
Cueto0725 <jeanclaudeleonard.cueto@gmail.com>
2025-05-08 21:58:58 +0800

    a

--- static/js/project.js
+++ static/js/project.js
@@ -8,25 +8,25 @@ document.addEventListener("DOMContentLoaded", function () {
         'thesis': {
             title: "A Portable Tracking and Safety Device for the Students of Lucsuhin Integrated School",
             description: "My thesis project involves developing a portable tracking and safety device integrated with a microcontroller. Along with the device, I created three applications tailored for parents, teachers, and police, allowing them to monitor children's location and camera in real-time. The device also includes an emergency button that, when triggered, sends an alert to the parent. From there, the parent can contact the police for assistance, who can then track the child's location using the integrated location tracker. This system is designed to ensure the safety of students, providing quick responses in emergency situations.",
-            image: "../static/images/Picture2.jpg",
+            image: "/static/images/Picture2.jpg",
             alt: "Thesis Project Image"
         },
         'cash-invoice': {
             title: "Cash Invoice System",
             description: "During my internship, I served as a Full Stack Developer in the development of a Cash Invoice System. This system was designed to streamline the invoicing processes of three different companies, eliminating the need for manual copying and pasting. As a result, employees were able to work more efficiently and with fewer errors. I utilized JavaScript, Python, HTML, and CSS for the frontend and backend, and used MySQL to manage the system’s database.",
-            image: "../static/images/cashinvoice.jpeg",
+            image: "/static/images/cashinvoice.jpeg",
             alt: "Cash Invoice Image"
         },
         'purchasing-system': {
             title: "Purchasing System",
             description: "Together with my co-workers during my internship, we developed a Purchasing System for the company, designed to manage data for various clients and integrate seamlessly with a Web App. I contributed to both the front-end and back-end of the project—designing the user interface and implementing functional components for smooth user interaction. On the back end, I worked with Python and MySQL, integrating the system through APIs and AJAX using JavaScript. Through this project, I gained valuable hands-on experience in full stack development and improved my skills in building dynamic, database-driven web applications.",
-            image: "../static/images/purchasing.jpg",
+            image: "/static/images/purchasing.jpg",
             alt: "Purchasing System Image"
         },
         'taps': {
             title: "TAPS",
             description: "The name of our web app is TAPS this is where the magic begins. It serves as the central hub for all the systems we built for the company, from the smallest details to the most complex features. We ensured that all the necessary data they need is stored here.",
-            image: "../static/images/bg.jpg",
+            image: "/static/images/bg.jpg",
             alt: "Taisei"
         }
     };
